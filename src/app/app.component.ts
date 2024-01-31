e29652afe51f0d54076c0e6d0b6c09cafabe2c92
Łukasz <Solik>
2024-01-31 21:20:56 +0100

    Add time picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { FormComponent } from './form-component/form-component.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  standalone: true,
-  imports: [FormComponent]
 })
 export class AppComponent {
   title = 'universe-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form-component/form-component.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
   imports: [
+    FormComponent,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [MatDatepickerModule],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
