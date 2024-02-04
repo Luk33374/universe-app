419e4005af0c2ef8a91cdb9e6909937d166c4ba8
Łukasz <Solik>
2024-02-04 13:31:48 +0100

    Add button

--- src/app/form-component/form-component.component.ts
+++ src/app/form-component/form-component.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -17,7 +17,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatIconModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatButton
   ],
   templateUrl: './form-component.component.html',
   styleUrl: './form-component.component.scss'
@@ -26,7 +27,15 @@ export class FormComponent {
   protected latitude = '';
   protected longitude = '';
   protected time = '';
-  protected date = new Date();
+  protected date:any = new Date();
   protected latitudeRegex = /[NSns][0-9]{1,2}[.][0-9]{6}/g;
   protected longitudeRegex = /[EWew][0-9]{1,2,3}[.][0-9]{6}/g;
+
+  private conunctionDate = Date.parse('01 Jan 1970 00:00:00 GMT');
+  
+  protected onClick(): void{
+    const milisecondsToDaysConvertionRate = 86400000;
+    const dateDiff = Math.abs(this.date - this.conunctionDate)/ milisecondsToDaysConvertionRate;
+    console.log(dateDiff);
+  }
 }
